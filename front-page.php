e16b4e09181a23828dce10d58b44791c31379bcb
Emil Wibe Lolk <emilwibe@emils-imac.lan>
2018-12-17 12:44:12 +0100

    s

--- front-page.php
+++ front-page.php
@@ -1,9 +1,15 @@
 <?php get_header(); ?>
 
+  <div class="hero wrapper fw">
+
     <?php if(get_field('hero_image_index')) : ?>
 
-    <img src="<?php the_field('hero_image_index'); ?>" alt="" class="hero">
+    <img src="<?php the_field('hero_image_index'); ?>" alt="" class="hero preload">
 
     <?php endif; ?>
+
+  </div><!--/.hero.wrapper.fw-->
+
+    
 
 <?php get_footer(); ?>
