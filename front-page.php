e8c91baacb2ae3279642c82e13c406c8f6d3b341
Emil Wibe Lolk <emilwibe@emils-imac.lan>
2018-12-17 12:31:17 +0100

    s

--- front-page.php
+++ front-page.php
@@ -1,3 +1,9 @@
 <?php get_header(); ?>
 
+    <?php if(get_field('hero_image_index')) : ?>
+
+    <img src="<?php the_field('hero_image_index'); ?>" alt="" class="hero">
+
+    <?php endif; ?>
+
 <?php get_footer(); ?>
