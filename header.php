4b70583770291806c6495d1e36214fee04825f13
Emil Wibe Lolk <emilwibe@emils-imac.lan>
2018-12-17 11:11:54 +0100

    workspace setup

---
+++ header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html <?php planguage_attributes(); ?>>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <?php wp_head(); ?>
+</head>
+<body>
