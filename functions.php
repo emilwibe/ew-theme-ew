babca212275105f869fb1134f67a481c74bbc23e
Emil Wibe Lolk <emilwibe@emils-imac.lan>
2018-12-17 11:33:28 +0100

    headering

--- functions.php
+++ functions.php
@@ -1,2 +1,6 @@
 <?php
     add_theme_support('title-tag');
+
+    add_action('wp_enqueue_scripts', function(){
+        wp_enqueue_style('ew-styles', get_template_directory_uri() . '/dist/css/core.css', '', '', '');
+    });
