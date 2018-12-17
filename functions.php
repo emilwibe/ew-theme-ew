e1c9b8a42129e9c0cfc09da060518a4818c5a505
Emil Wibe Lolk <emilwibe@emils-imac.lan>
2018-12-17 12:05:13 +0100

    s

--- functions.php
+++ functions.php
@@ -1,6 +1,11 @@
 <?php
-    add_theme_support('title-tag');
+  add_theme_support('title-tag');
 
-    add_action('wp_enqueue_scripts', function(){
-        wp_enqueue_style('ew-styles', get_template_directory_uri() . '/dist/css/core.css', '', '', '');
-    });
+  add_action('wp_enqueue_scripts', function(){
+    wp_enqueue_style('ew-styles', get_template_directory_uri() . '/dist/css/core.css', '', '', '');
+  });
+
+  register_nav_menus(array(
+    'primary' => 'Primary Nav',
+  ));
+    
--- header.php
+++ header.php
@@ -10,6 +10,9 @@
   <body>
     <header class="header primary">
       <div class="wrapper lg2">
-        <a href="/" class="logo">EmilWibe <span>Web Design</span></a>
+        <a href="/" class="logo">EmilWibe <span>WebDesign</span></a>
+
+        <?php get_template_part('tp/nav', 'primary'); ?>
+
       </div><!--/.wrapper.lg-->
     </header><!--/.header.primary-->
