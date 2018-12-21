cfaf6788117aa83640cc0ad25543b4db23933a55
Emil Wibe Lolk <emilwibe@emils-imac.lan>
2018-12-21 10:18:10 +0100

    s

--- tp/index-posts.php
+++ tp/index-posts.php
@@ -10,14 +10,20 @@ $theQuery = new WP_Query($args);
 <?php if($theQuery->have_posts()) : ?>
     
     <h2>Seneste posts</h2>
+    <div class="row col-33">
     <?php while($theQuery->have_posts()) : $theQuery->the_post(); ?>
 
-        <article>
-            <a href="<?php echo get_the_permalink(); ?>">
-                <div class="thumbnail" style="background-image:url(<?php the_post_thumbnail_url(); ?>);"></div>
-            </a>
-        </article>
-
+        <div>
+            <div>
+            <article>
+                <a href="<?php echo get_the_permalink(); ?>">
+                    <div class="thumbnail" style="background-image:url(<?php the_post_thumbnail_url(); ?>);"></div>
+                </a>
+                </article>
+            </div>
+        </div>
+ 
     <?php endwhile; ?>
+    </div><!--/.row-->
     <?php wp_reset_postdata(); ?>
 <?php endif; ?>
