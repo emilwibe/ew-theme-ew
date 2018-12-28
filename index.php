07b2474f9089c9985c9a2676da8233c0b59e14d7
Emil Wibe Lolk <emilwibe@emils-imac.lan>
2018-12-28 13:24:39 +0100

    s

--- index.php
+++ index.php
@@ -7,12 +7,7 @@
     <div class="wrapper lg2 row lg-col-25-75 md-col-33-66 sm-col-100">
         <div>
             <div>
-            <?php if(is_active_sidebar('project_widgets')) : ?>
-
-<ul id="sidebar">
-    <?php dynamic_sidebar('project_widgets'); ?>
-</ul>
-<?php endif; ?>
+                
             </div>
         </div>
         <div>
