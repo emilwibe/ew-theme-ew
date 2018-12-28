5ef2bfdfbbe5bc84d3480c44a096e3c43c75e577
Emil Wibe Lolk <emilwibe@emils-imac.lan>
2018-12-28 12:42:42 +0100

    s

--- single-project.php
+++ single-project.php
@@ -12,7 +12,7 @@
     <div class="wrapper lg2 row lg-col-25-75 md-col-33-66 sm-col-100">
         <div>
             <div>
-                <?php get_sidebar(); ?>
+                <?php get_template_part('tp/widget', 'projects'); ?>
             </div>
         </div>
         <div>
