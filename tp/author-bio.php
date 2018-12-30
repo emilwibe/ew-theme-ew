83f9bf6f36cec2a3f9d6553e8dafe0cc0cdbcf8a
Emil Wibe Lolk <emilwibe@emils-imac.lan>
2018-12-30 20:51:03 +0100

    s

--- tp/author-bio.php
+++ tp/author-bio.php
@@ -1,4 +1,4 @@
-<img alt='' src='https://secure.gravatar.com/avatar/70cf24e98300ae5b0d394a24564f50e6?s=300&#038;d=mm&#038;r=g' srcset='https://secure.gravatar.com/avatar/70cf24e98300ae5b0d394a24564f50e6?s=600&#038;d=mm&#038;r=g 2x' class='avatar avatar-300 photo' height='300' width='300' /></div>
+<img alt='' src='https://secure.gravatar.com/avatar/70cf24e98300ae5b0d394a24564f50e6?s=300&#038;d=mm&#038;r=g' srcset='https://secure.gravatar.com/avatar/70cf24e98300ae5b0d394a24564f50e6?s=600&#038;d=mm&#038;r=g 2x' class='avatar avatar-300 photo' height='300' width='300' />
 <div class="wysiwyg">
   <h4>Emil Wibe</h4>
   <p>Front- / backend webudvikler, WordPress-udvikler, oplægsholder, lærer</p>
