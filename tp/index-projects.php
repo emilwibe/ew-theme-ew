d11ee253164506ef61acc6f9337d1ae184197f47
Emil Wibe Lolk <emilwibe@emils-imac.lan>
2018-12-30 15:49:30 +0100

    s

--- tp/index-projects.php
+++ tp/index-projects.php
@@ -8,10 +8,9 @@
         setup_postdata( $post ); 
     
 ?>
-<div class="wrapper lg2">
-    <h2 class="index posts">Udvalgt Projekt</h2>
-</div>
-<div class="wrapper lg2 row lg-col-75-25 md-col75-25 sm-col-100">
+
+<h2 class="index posts">Udvalgt Projekt</h2>
+<div class="row lg-col-75-25 md-col75-25 sm-col-100">
     <div>
         <div>
             <h3><a href="<?php echo get_the_permalink(); ?>"><?php the_title(); ?></a></h2>
