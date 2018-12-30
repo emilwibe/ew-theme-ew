00e09fd8be538a08dcb5fec9ea912cf408955508
Emil Wibe Lolk <emilwibe@emils-imac.lan>
2018-12-30 21:35:19 +0100

    s

---
+++ tp/portfolio-featured.php
@@ -0,0 +1,30 @@
+<?php if(have_rows('project_featured_portfolio')): ?>
+  <div class="row all-gutter-zero lg-col-33 md-col-33 portfolio-archive">
+    <?php while (have_rows('project_featured_portfolio')) : the_row(); ?>
+
+<?php
+
+$post_object = get_sub_field('object_project_featured_portfolio');
+
+if($post_object) :
+  $post = $post_object;
+	setup_postdata($post);
+?>
+  <article>
+    <div>
+      <div>
+        <a href="<?php echo get_the_permalink(); ?>">
+          <div class="overlay">
+            <h2><?php the_title(); ?></h2>
+            <p><?php the_field('client_project'); ?></p>
+          </div><!--/.overlay-->
+          <?php the_post_thumbnail(); ?>
+        </a>
+      </div>
+    </div>
+  </article>
+<?php wp_reset_postdata(); ?>
+  <?php endif; ?>
+    <?php endwhile; ?>
+  </div><!--/.row.col50-->
+<?php endif; ?>
