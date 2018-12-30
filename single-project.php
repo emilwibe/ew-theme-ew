fb87c600c84d50bcbdc56465d096937268e6c08f
Emil Wibe Lolk <emilwibe@emils-imac.lan>
2018-12-30 20:34:40 +0100

    s

--- single-project.php
+++ single-project.php
@@ -12,13 +12,13 @@
     <div class="wrapper lg2 row lg-col-25-75 md-col-33-66 sm-col-100">
         <div>
             <div>
-                <?php get_template_part('tp/single', 'aside'); ?>
                 <?php if(is_active_sidebar('project_widgets')) : ?>
 
                 <ul id="sidebar">
                     <?php dynamic_sidebar('project_widgets'); ?>
                 </ul>
                 <?php endif; ?>
+                <?php get_template_part('tp/single', 'aside'); ?>
             </div>
         </div>
         <div>
