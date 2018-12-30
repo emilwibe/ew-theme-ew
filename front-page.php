e0ecc220b9ea91aed2f099c39a188fe26f064d15
Emil Wibe Lolk <emilwibe@emils-imac.lan>
2018-12-30 15:39:24 +0100

    s

--- front-page.php
+++ front-page.php
@@ -3,6 +3,8 @@
   <?php get_template_part('tp/image', 'featured'); ?>
   
   <main>
+
+    <?php get_template_part('tp/index', 'projects'); ?>
     
     <?php get_template_part('tp/index', 'posts'); ?>
 
