0a812b1f7316b0a8239f7a1b1d562bd87e241b3c
Emil Wibe Lolk <emilwibe@emils-imac.lan>
2018-12-30 16:37:12 +0100

    s

--- front-page.php
+++ front-page.php
@@ -1,8 +1,21 @@
 <?php get_header(); ?>
 
   <?php get_template_part('tp/image', 'featured'); ?>
+
+  <main>
   
-  <main class="wrapper lg2 row lg-col-75-25 md-col-66-33 sm-col-100">
+<?php if(have_posts()) : ?>
+
+  <div class="wrapper sm">
+  <?php while(have_posts()) : the_post(); ?>
+
+    <?php the_content(); ?>
+
+  <?php endwhile; ?>
+  </div><!--/.wrapper.sm-->
+<?php endif; ?>
+
+  <div class="wrapper lg2 row lg-col-75-25 md-col-66-33 sm-col-100">
 
     <div>
       <div>
@@ -15,6 +28,7 @@
       </div>
     </div>
 
-  </main>
+</div><!--/.row-->
 
+</main>
 <?php get_footer(); ?>
