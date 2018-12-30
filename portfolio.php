71254f35d772871471f47e2bad1950949e256459
Emil Wibe Lolk <emilwibe@emils-imac.lan>
2018-12-30 21:23:02 +0100

    s

--- portfolio.php
+++ portfolio.php
@@ -2,11 +2,15 @@
 <?php get_header(); ?>
 
     <?php if(have_posts()) : ?>
+
+    <div class="wrapper sm">
         <?php while(have_posts()) : the_post(); ?>
 
             <?php the_content(); ?>
 
         <?php endwhile; ?>
+
+    </div><!--/.wrapper-->
     <?php endif; ?>
 
 <?php get_footer(); ?>
