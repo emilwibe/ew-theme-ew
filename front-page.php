1669fbe7a3d6ce94b82b6b649a50ad2c04b95c42
Emil Wibe Lolk <emilwibe@emils-imac.lan>
2018-12-30 15:48:16 +0100

    s

--- front-page.php
+++ front-page.php
@@ -2,8 +2,8 @@
 
   <?php get_template_part('tp/image', 'featured'); ?>
   
-  <main class="wrapper lg2 row .lg-col-75-25.md-col-75-25.sm-col-100">
-  
+  <main class="wrapper lg2">
+
     <div>
       <div>
         <?php get_template_part('tp/index', 'projects'); ?>
