64aefdfb474937ddd6c40e5dc4b03bce575f9a08
Emil Wibe <mail@emilwibe.dk>
2019-01-08 14:06:42 +0100

    s

--- archive.php
+++ archive.php
@@ -1,26 +1,11 @@
 <?php /* Template Name: Blog Page */ ?>
 <?php get_header(); ?>
-
-<header class="page-header">
-    <?php
-        the_archive_title( '
- 
-<h1 class="page-title">', '</h1>
- 
- 
-' );
-        the_archive_description( '
- 
-<div class="taxonomy-description">', '</div>
- 
- 
-' );
-    ?>
-</header>
 <?php if(have_posts()) : ?>
     <?php while(have_posts()) : the_post(); ?>
 
-    <div class="wrapper sm row lg-col-25-75 md-col-33-66 sm-col-100">
+    <?php get_template_part('tp/image', 'featured'); ?>
+
+    <div class="wrapper xl row lg-col-25-75 md-col-33-66 sm-col-100">
         <div>
             <div>
                 
@@ -28,7 +13,7 @@
         </div>
         <div>
             <div>
-                <main><?php the_excerpt(); ?></main>
+                <main><?php the_content(); ?></main>
             </div>
         </div>
     </div><!--/.row-->
