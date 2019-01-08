b301f64eb577da37b50838dda96b387382d37b3c
Emil Wibe <mail@emilwibe.dk>
2019-01-08 11:35:25 +0100

    s

--- archive.php
+++ archive.php
@@ -1,5 +1,22 @@
 <?php /* Template Name: Blog Page */ ?>
 <?php get_header(); ?>
+
+<header class="page-header">
+    <?php
+        the_archive_title( '
+ 
+<h1 class="page-title">', '</h1>
+ 
+ 
+' );
+        the_archive_description( '
+ 
+<div class="taxonomy-description">', '</div>
+ 
+ 
+' );
+    ?>
+</header>
 <?php if(have_posts()) : ?>
     <?php while(have_posts()) : the_post(); ?>
 
