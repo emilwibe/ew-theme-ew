bf39ab2a0552506fd1848cbba3fd6160094b5966
Emil Wibe <mail@emilwibe.dk>
2019-01-18 09:59:13 +0100

    s

--- index.php
+++ index.php
@@ -5,12 +5,19 @@
     <?php get_template_part('tp/image', 'featured'); ?>
 
     <div class="wrapper xl row lg-col-25-75 md-col-33-66 sm-col-100">
-        <div>
+        <div class="sm-order-2">
             <div>
-                
+                <?php get_template_part('tp/author', 'bio'); ?>
+
+                <?php if(is_active_sidebar('project_widgets')) : ?>
+
+                <ul id="sidebar">
+                    <?php dynamic_sidebar('project_widgets'); ?>
+                </ul>
+                <?php endif; ?>
             </div>
         </div>
-        <div>
+        <div class="sm-order-1">
             <div>
                 <main><?php the_content(); ?></main>
             </div>
