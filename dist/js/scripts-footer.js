041228ee2f5504d5c2a6645ac86963a3b5523c5b
Emil Wibe <mail@emilwibe.dk>
2019-02-10 21:38:40 +0100

    s

--- dist/js/scripts-footer.js
+++ dist/js/scripts-footer.js
@@ -1,6 +1,8 @@
 (function(){
     var navPrimary = document.getElementById("nav-primary"),
-        navToggle = document.getElementById("nav-toggle")
+        navToggle = document.getElementById("nav-toggle"),
+        hero = document.getElementById("hero"),
+        heroPreload = document.getElementById("hero-preload")
     ;
 
     navToggle.addEventListener("click", function(e){
@@ -8,6 +10,10 @@
         console.log(navToggle);
         e.target.classList.toggle("is-active");
         navPrimary.classList.toggle("is-hidden");
+    }, false);
+
+    heroPreload.addEventListener("load", function(){
+        hero.style.animationPlayState = "running";
     }, false);
 })();
 /* -----------------------------------------------
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,6 +1,8 @@
 (function(){
     var navPrimary = document.getElementById("nav-primary"),
-        navToggle = document.getElementById("nav-toggle")
+        navToggle = document.getElementById("nav-toggle"),
+        hero = document.getElementById("hero"),
+        heroPreload = document.getElementById("hero-preload")
     ;
 
     navToggle.addEventListener("click", function(e){
@@ -8,5 +10,9 @@
         console.log(navToggle);
         e.target.classList.toggle("is-active");
         navPrimary.classList.toggle("is-hidden");
+    }, false);
+
+    heroPreload.addEventListener("load", function(){
+        hero.style.animationPlayState = "running";
     }, false);
 })();
